c215e7f89df58a6f38b2a0834b502273f77fd215
Kristiyan Hristov <kristiyan.hristov@zigzag.global>
2024-11-25 13:26:41 +0200

    implementing initial components with tailwind styling

---
+++ 06-project-management-app/src/components/Project.jsx
@@ -0,0 +1,9 @@
+export default function Project() {
+  return (
+      <li className="flex justify-between my-4">
+        <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+          Project 1
+        </button>
+      </li>
+  );
+}
