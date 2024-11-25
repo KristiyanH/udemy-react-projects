10b4f12b77f8c7280d05bd8438a3c7a767fc5e62
Kristiyan Hristov <kristiyan.hristov@zigzag.global>
2024-11-25 15:29:08 +0200

    collecting input with refs, adding projects functionality

--- 06-project-management-app/src/App.jsx
+++ 06-project-management-app/src/App.jsx
@@ -1,11 +1,57 @@
-import NewProject from './components/NewProject.jsx';
-import SideBar from './components/SideBar.jsx';
+import { useState, useRef } from "react";
+
+import NewProject from "./components/NewProject.jsx";
+import SideBar from "./components/SideBar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const projectInput = useRef();
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    //save project
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+  
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAddProject={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NewProject />
+      <SideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
