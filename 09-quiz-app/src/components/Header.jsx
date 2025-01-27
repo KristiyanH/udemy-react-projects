773037dda9051202cd568915bffe123245d7f4b7
Kristiyan Hristov <kristiyan.hristov@zigzag.global>
2025-01-27 18:03:02 +0200

    adding header and quiz components

---
+++ 09-quiz-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logoImage from "../assets/quiz-logo.png";
+
+export default function Header(){
+    return <header>
+        <img src={logoImage} alt="Quiz logo"></img>
+        <h1>ReactQuiz</h1>
+    </header>
+}
