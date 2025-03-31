81dd5f3f6fe1058445b06f704ef32293d7b49134
Kristiyan Hristov <kristiyan.hristov@zigzag.global>
2025-03-31 16:09:32 +0300

    resetting form upon submit

--- 13-forms/src/components/StateLogin.jsx
+++ 13-forms/src/components/StateLogin.jsx
@@ -6,10 +6,51 @@ export default function StateLogin() {
     password: "",
   });
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(enteredCredentials);
+  }
+
   function handleCredentialsInputChange(identifier, value) {
     setEnteredCredentials((prevCreds) => ({
       ...prevCreds,
       [identifier]: value,
     }));
   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredCredentials.email}
+            onChange={handleCredentialsInputChange}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredCredentials.password}
+            onChange={handleCredentialsInputChange}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
 }
